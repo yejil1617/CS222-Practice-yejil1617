212fb1e9680b0539187229e17acee8adef0a5fea
yejil1617 <90877301+yejil1617@users.noreply.github.com>
2022-10-06 20:49:58 -0500

    Router, logger, morgan, Postman

--- DevCamper_API/server.js
+++ DevCamper_API/server.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
 
 // Load config files
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// Dev loggin middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Mount rothers
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 4800;
 
